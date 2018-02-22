b68870502acc70b8e5632710a015e5d49292e206
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2018-02-22 00:20:09 -0600

    Add-Start Node server configuration-AlexisNava

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+// Dependencies
+const express = require('express');
+const path = require('path');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// App Instance
+const app = express();
+
+// Enviroment Variables
+const PORT = process.env.PORT || 5000;
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+// Static Folder
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Send All Trafic to React
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.hmtl'));
+});
+
+// Listen Port
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
