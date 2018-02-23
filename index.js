09cf88a2d77fce3fdff30045d8562717ce5e5212
ak900s <ak900s@yahoo.com.mx>
2018-02-23 02:27:28 -0600

    update

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express();
+
+
+  app.use(express.static(path.join(__dirname, 'public')));
+
+ 
+  app.get('/',function(req, res){
+      res.send("hello world");
+  });
+  
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
