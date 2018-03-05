00267c7f336319ec2dbea8de0dec7007828b4540
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2018-03-04 23:31:23 -0600

    Add-Add templates and app files-AlexisNava-04/03/2018

---
+++ src/app/containers/App/index.jsx
@@ -0,0 +1,10 @@
+import { PureComponent } from 'react';
+import MuiThemeProvider from 'material-ui';
+
+const App = () => (
+	<MuiThemeProvider>
+		<h1>Hello World!</h1>
+	</MuiThemeProvider>
+);
+
+export default App;
---
+++ src/app/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import App from './containers/App/index.jsx';
+
+render(<App />, document.getElementById('root'));
