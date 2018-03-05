9ee0d9471b52e566fc29b51052c1c969065df9ab
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2018-03-04 23:20:27 -0600

    Update-Change server configuration-AlexisNava-04/03/2018

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,14 @@
+/* eslint no-console: 0 */
+
 // Dependencies
 const express = require('express');
 const path = require('path');
+const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddlewar = require('webpack-hot-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
 
 // Webpack development configuration file
-const webpackConfig = require('./src/webpack/webpack.config.dev');
+const webpackConfig = require('../webpack/webpack.config.dev');
 
 // Compile Webpack
 const webpackCompile = webpack(webpackConfig);
@@ -15,20 +18,27 @@ const app = express();
 
 // Enviroment Variables
 const PORT = process.env.PORT || 5000;
-const isDevelopment = process.env.NODE_ENV !== 'production';
+const isDevelopment = process.env.NODE_ENV === 'development';
+const isProduction = process.env.NODE_ENV === 'production';
 
 if (isDevelopment) {
   app.use(webpackDevMiddleware(webpackCompile));
-  app.use(webpackHotMiddleware(webpackCompile));  
-}
-
+  app.use(webpackHotMiddleware(webpackCompile));
+} else if (isProduction) {
 // Static Folder
-app.use(express.static(path.join(__dirname, 'dist')));
+  app.use(express.static(path.join(__dirname, 'dist')));
 
-// Send All Trafic to React
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.hmtl'));
-});
+  // Send All Trafic to React
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.hmtl'));
+  });
+} else {
+  try {
+    throw new Error('Please verify your NODE_ENV variable');
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 // Listen Port
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
